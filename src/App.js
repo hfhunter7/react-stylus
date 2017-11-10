a1d36070a60f9f5a114a7758ad9e65714c2def66
sivakorn <sivakorn@9picompany.com>
2017-11-10 16:28:04 +0700

    create layout training center

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Container } from "./style-js/grid";
+import tcIcon from './icon_tc.png';
+import GoogleButton from 'react-google-button';
 
 class App extends Component {
     render() {
         return (
-            <Container onClick={() => alert("OK")}>
-                Hello
-            </Container>
+            <div>
+                <table className="border-table2">
+                    <tr>
+                        <td className="bg-color"><p></p></td>
+                    </tr>
+                </table>
+                <table className="border-table">
+                    <tr>
+                        <td className="icon-tc"><img className="img" src={tcIcon}/></td>
+                    </tr>
+                    <tr>
+                        <td className="text-tc">Training Center</td>
+                    </tr>
+                    <tr>
+                        <td className="text-content">
+                            <p>
+                                Online training courses with certificates that can help you <br/>
+                                build your skills and advance your career!
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="icon-center">
+                            <i className="material-icons icon-margin1">play_circle_filled</i>
+                            <i className="material-icons icon-margin2">assignment</i>
+                            <i className="material-icons icon-margin3">help_outline</i>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="btn-center">
+                            <center><GoogleButton /></center><br /><br />
+                        </td>
+                    </tr>
+                </table>
+            </div>
         )
     }
 }
